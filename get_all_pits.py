5a40b6fe7e6c7eebdfeb63c3baec8f1b01343744
Hans-Peter Marshall <hpmarshall@boisestate.edu>
2017-09-13 00:29:25 -0600

    fix typo on UTM coor in M22 and remove corrupted 39S

---
+++ get_all_pits.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 22:42:43 2017
+
+@author: hpm
+"""
+#from IPython import get_ipython
+#get_ipython().magic('reset -sf')
+
+import pandas as pd
+import glob
+
+def readSnowpit(filename):
+    #filename='Pit_Output/2017-02-06/PIT_L36/pit_20170206_L36.xlsx'
+    print(filename)
+    xl = pd.ExcelFile(filename)
+    data=pd.read_excel(xl,sheetname=0,parse_cols='H')
+    UTMN=int(data['UTMN:'][0])
+    UTME=int(data['UTMN:'][2])
+    UTMzone=data['UTMN:'][4]
+    density=pd.read_excel(xl,sheetname=0,header=8,parse_cols='E:F')
+    mean_density=density.dropna().mean().mean()
+    data=pd.read_excel(xl,sheetname=0,header=0,parse_cols='F')
+    depth=float(data['Total Depth (cm)'][0])
+    data=pd.read_excel(xl,sheetname=0,header=0,parse_cols='M')
+    pit_date=data['Surveyors:'][2]
+    data=pd.read_excel(xl,sheetname=0,header=0,parse_cols='O')
+    pit_time=data['Time:'][0]
+    SWE=depth*mean_density/1000
+    r=pd.DataFrame(columns={'UTM_N','UTM_E','UTM_zone','date','time','depth','density','SWE'})
+    r.set_value(0,'UTM_N',UTMN)
+    r.set_value(0,'UTM_E',UTME)
+    r.set_value(0,'UTM_zone',UTMzone)
+    r.set_value(0,'date',pit_date)
+    r.set_value(0,'time',pit_time)
+    r.set_value(0,'depth',depth)
+    r.set_value(0,'density',mean_density)
+    r.set_value(0,'SWE',SWE)
+    return r
+
+
+
+
+#mylist={'UTMN':UTMN,'UTME':UTME,'UTMzone':UTMzone}
+#print(mylist)
+
+    # xl.sheet_names
+
+
+
+
+pits=pd.DataFrame(columns={'UTM_N','UTM_E','UTM_zone','date','time','depth','density','SWE'})
+for filename in glob.iglob('./Pit_output/**/*.xlsx', recursive=True): # loop over all GPS files
+    r=readSnowpit(filename)
+    pits=pits.append(r,'ignore_index=True') # append to pits data frame the current pit
+    
+    # function to parse pit data
+
+
+#os.chdir('Pit_Output/')
+#result = glob.glob( '*/**.xlsx' )
+#print( result )
